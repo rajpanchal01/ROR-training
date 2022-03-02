09b3ac723c1e8e20fdbc2525cbcd087544b97473
rajpanchal01 <rajpanchal2810@gmail.com>
2022-03-02 17:30:16 +0530

    File I/O operations(updated)

--- session/14-02/questions.rb
+++ session/14-02/questions.rb
@@ -5,6 +5,11 @@ rajpanchal01@rajpanchal01-Aspire-A515-43:~/Desktop/ROR-training/session/14-02$ r
 there is 2 lines here.
 #there is text file in ruby command so it is syntax  error .
 
-#2.
+#2. Why readlines was not printing \n?
+
+#- In programming languages, such as C, Java, and Ruby, the newline character is represented as a '\n' which is an escape sequence.
+#- A newline is a character used to represent the end of a line of text and the beginning of a new line.
+#- So whenever ruby compiler encounters '\n', it moves the cursor to next line. However, if you
+#  specifically want to print '\n' to output screen then you would have to right like this '\\n'.
 
 
